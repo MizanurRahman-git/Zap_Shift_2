53afb04a6019f93320d10364a6baaaab708fa06d
Mizanur Rahman <mdmizanurrahmanhridoy5@gmail.com>
2026-07-31 10:34:04 +0600

    added more errors handling

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,30 +1,54 @@
 import { useForm } from "react-hook-form";
 
-
 const Register = () => {
-    const {register, handleSubmit, formState:{errors}} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    const handleRegistration = (data) => {
-        console.log(data)
-    }
+  const handleRegistration = (data) => {
+    console.log(data);
+  };
   return (
     <div>
       <form onSubmit={handleSubmit(handleRegistration)}>
         <fieldset className="fieldset">
           <label className="label">Email</label>
-          <input type="email" {...register("email", {required:true})} className="input" placeholder="Email" />
-          {
-            errors.email?.type === "required" && <p className="text-red-400">Email is Required</p>
-          }
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="input"
+            placeholder="Email"
+          />
+          {errors.email?.type === "required" && (
+            <p className="text-red-400">Email is Required</p>
+          )}
           {/* password */}
           <label className="label">Password</label>
-          <input type="password" {...register("password", {required:true, minLength:6})} className="input" placeholder="Password" />
-          {
-            errors.password?.type === "required" && <p className="text-red-400">Password is Required</p>
-          }
-          {
-            errors.password?.type === "minLength" && <p className="text-red-400">Password must be 6 digits.</p>
-          }
+          <input
+            type="password"
+            {...register("password", {
+              required: true,
+              minLength: 6,
+              pattern:
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{6,}$/,
+            })}
+            className="input"
+            placeholder="Password"
+          />
+          {errors.password?.type === "required" && (
+            <p className="text-red-400">Password is Required</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-400">Password must be 6 digits.</p>
+          )}
+          {errors.password?.type === "pattern" && (
+            <p className="text-red-300">
+              Password will be must at least one uppercase, at least one lowercase, at
+              least one number, and at least one special character.
+            </p>
+          )}
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
