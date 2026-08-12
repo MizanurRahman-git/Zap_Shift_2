eeec2e6e57579526f7d0998c366444ddd462feca
Mizanur Rahman <mdmizanurrahmanhridoy5@gmail.com>
2026-08-12 22:46:59 +0600

    added table for my parcels

--- src/pages/Dashboard/MyParcel/MyParcel.jsx
+++ src/pages/Dashboard/MyParcel/MyParcel.jsx
@@ -3,22 +3,48 @@ import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const MyParcel = () => {
-    const {user} = useAuth()
-    const axiosSecure = useAxiosSecure()
-    const {data} = useQuery({
-        queryKey:['myParcels', user?.email],
-        queryFn: async() => {
-            const res = await axiosSecure.get(`/parcels?email=${user?.email}`)
-            return res.data
-        }
-    })
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data } = useQuery({
+    queryKey: ["myParcels", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/parcels?email=${user?.email}`);
+      return res.data;
+    },
+  });
 
-    return (
-        <div>
-            <h2>This is My parcel page: {data?.length}</h2>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h2>This is My parcel page: {data?.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Cost</th>
+              <th>Payment Stutus</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((parcel, index) => (
+              <tr key={parcel._id}>
+                <th>{index + 1}</th>
+                <td>{parcel.parcelName}</td>
+                <td>{parcel.cost}</td>
+                <td>Blue</td>
+                <td>
+                    <button className="btn">delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyParcel;
